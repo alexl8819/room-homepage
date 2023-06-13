3c5e73c7644cbede0a87b5352792df5668241040
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-06-13 14:18:07 -0700

    ui: fixes missing bg image on initial load, refactor: renames marker to cursor in slider to describe list index selected

--- src/slider.js
+++ src/slider.js
@@ -2,20 +2,21 @@ import * as sliderItems from './slider-items.json';
 
 document.addEventListener('DOMContentLoaded', () => {
   const items = new Set(sliderItems || []); /* Represent slider items as a set */
-  let marker = 0;
+  let cursor = 0;
   // Bind next/prev buttons
   const nextBtn = document.querySelector('.slider__next');
   const prevBtn = document.querySelector('.slider__prev');
   const itemArr = Array.from(items);
+  // Setup listeners
   nextBtn.addEventListener('click', () => {
-    marker = marker >= items.size - 1 ? 0 : ++marker;
-    applyContent(itemArr[marker]);
+    cursor = cursor >= items.size - 1 ? 0 : ++cursor;
+    applyContent(itemArr[cursor]);
   });
   prevBtn.addEventListener('click', () => {
-    marker = marker <= 0 ? items.size - 1 : --marker;
-    applyContent(itemArr[marker]);
+    cursor = cursor <= 0 ? items.size - 1 : --cursor;
+    applyContent(itemArr[cursor]);
   });
-  applyContent(itemArr[marker]);
+  applyContent(itemArr[cursor]);
 });
 
 function applyContent (selected) {
@@ -28,7 +29,6 @@ function applyContent (selected) {
   for(const [_, className] of heroEl.classList.entries()) {
     if (className.indexOf('bg-mobile-hero') > -1) {
       heroEl.classList.remove(className);
-      console.log('removing ' + className);
     }
   }
 
@@ -36,6 +36,7 @@ function applyContent (selected) {
 
   const headingEl = document.querySelector('.slider__heading');
   const contentEl = document.querySelector('.slider__content');
+
   headingEl.innerText = selected.heading;
   contentEl.innerText = selected.content;
 }

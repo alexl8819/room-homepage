6940e306c0824ab832700371271dce92a3f5e1e5
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2023-07-04 19:12:30 -0700

    implementation: add initial desktop viewport layout, add support for both desktop and mobile images in slider

--- src/slider.js
+++ src/slider.js
@@ -1,52 +1,61 @@
 import * as sliderItems from './slider-items.json';
 
-import heroImageOne from './images/mobile-image-hero-1.jpg';
-import heroImageTwo from './images/mobile-image-hero-2.jpg';
-import heroImageThree from './images/mobile-image-hero-3.jpg';
+import mbHeroImageOne from './images/mobile-image-hero-1.jpg';
+import dsHeroImageOne from './images/desktop-image-hero-1.jpg'
+import mbHeroImageTwo from './images/mobile-image-hero-2.jpg';
+import dsHeroImageTwo from './images/desktop-image-hero-2.jpg';
+import mbHeroImageThree from './images/mobile-image-hero-3.jpg';
+import dsHeroImageThree from './images/desktop-image-hero-3.jpg';
 
 document.addEventListener('DOMContentLoaded', () => {
   const items = new Set(sliderItems || []); /* Represent slider items as a set */
   let cursor = 0;
+  let isLargeViewport = false;
   // Bind next/prev buttons
-  const nextBtn = document.querySelector('.slider__next');
-  const prevBtn = document.querySelector('.slider__prev');
+  const nextBtn = document.querySelector('#slider-next');
+  const prevBtn = document.querySelector('#slider-prev');
   const itemArr = Array.from(items);
   // Setup listeners
   nextBtn.addEventListener('click', () => {
     cursor = cursor >= items.size - 1 ? 0 : ++cursor;
-    applyContent(itemArr[cursor]);
+    applyContent(itemArr[cursor], isLargeViewport);
   });
   prevBtn.addEventListener('click', () => {
     cursor = cursor <= 0 ? items.size - 1 : --cursor;
-    applyContent(itemArr[cursor]);
+    applyContent(itemArr[cursor], isLargeViewport);
   });
-  applyContent(itemArr[cursor]);
+  window.addEventListener('resize', () => {
+    isLargeViewport = window.innerWidth >= 768 ? true : false;
+    applyContent(itemArr[cursor], isLargeViewport);
+  });
+  isLargeViewport = window.innerWidth >= 768 ? true : false;
+  applyContent(itemArr[cursor], isLargeViewport);
 });
 
-function applyContent (selected) {
+function applyContent (selected, isLargeViewport) {
   if (!selected) {
     throw new Error('Invalid item selected: null or undefined');
   }
   
-  const heroEl = document.querySelector('.slider__hero');
+  const heroEl = document.querySelector('#slider-hero');
   let imgUrl;
 
   switch (selected.hero) {
     case 1:
-      imgUrl = `url(${heroImageOne})`;
+      imgUrl = `url(${!isLargeViewport ? mbHeroImageOne : dsHeroImageOne})`;
       break;
     case 2:
-      imgUrl = `url(${heroImageTwo})`;
+      imgUrl = `url(${!isLargeViewport ? mbHeroImageTwo : dsHeroImageTwo})`;
       break;
     case 3:
-      imgUrl = `url(${heroImageThree})`;
+      imgUrl = `url(${!isLargeViewport ? mbHeroImageThree : dsHeroImageThree})`;
       break;
   }
 
   heroEl.style.backgroundImage = imgUrl;
 
-  const headingEl = document.querySelector('.slider__heading');
-  const contentEl = document.querySelector('.slider__content');
+  const headingEl = document.querySelector('#slider-heading');
+  const contentEl = document.querySelector('#slider-content');
 
   headingEl.innerText = selected.heading;
   contentEl.innerText = selected.content;

876c6662f75b2a82b7510e3c4a0b40e109951785
alexl8819 <124635195+alexl8819@users.noreply.github.com>
2024-02-18 02:42:25 +0000

    Deploying to gh-pages from @ alexl8819/room-homepage@3e5f642a9ca814b3c32e542d40728223673964f7 🚀

---
+++ index.a03220c2.js
@@ -0,0 +1,2 @@
+window.addEventListener("resize",()=>{let e=document.querySelector('input[type="checkbox"]');window.innerWidth>=768&&e.checked&&(e.checked=!1)});
+//# sourceMappingURL=index.a03220c2.js.map
